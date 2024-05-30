126f1f4cf925535f14886b9b9691c963348af01a
suryagowdaa <169018873+suryagowdaa@users.noreply.github.com>
2024-05-30 18:22:02 +0530

    Update SimpleApplication.java

--- src/main/java/com/example/simple/SimpleApplication.java
+++ src/main/java/com/example/simple/SimpleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello india");
+		System.out.println("hello indiaa");
 		SpringApplication.run(SimpleApplication.class, args);
 	}
 
